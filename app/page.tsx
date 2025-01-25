54ae786dd048f74cf443397eda6cf08a6383d026
anan <nut999anan@hotmail.com>
2025-01-25 18:36:47 +0700

    working on project components

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Skills from "@/components/main/Skills";
+import Projects from "@/components/main/Projects";
 import { Profile } from "@/components/main/Profile";
 import { Background } from "@/components/main/Background";
 import { ThemeToggleButton } from "@/components/ThemeToggleButton";
@@ -15,8 +16,9 @@ export default function Home() {
       <div className="w-8/12 mx-auto items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 relative">
         <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start overflow-hidden p-2">
           <Profile />
-          <Background />
           <Skills />
+          <Projects />
+          <Background />
         </main>
       </div>
     </>
---
+++ components/main/Projects.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="text-3xl font-bold">Projects</h1>
+      <div></div>
+    </div>
+  );
+};
+
+export default Projects;
