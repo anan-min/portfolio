cbbc3faae4e008b300d37b82da2b06b4c01d85a7
anan <nut999anan@hotmail.com>
2025-01-25 18:27:11 +0700

    skills update

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,24 @@
 "use client";
+import Skills from "@/components/main/Skills";
 import { Profile } from "@/components/main/Profile";
-import { Meteors } from "@/components/ui/meteors";
 import { Background } from "@/components/main/Background";
 import { ThemeToggleButton } from "@/components/ThemeToggleButton";
+import { Meteors } from "@/components/ui/meteors";
 
 export default function Home() {
   return (
-    <div className="w-8/12 mx-auto items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 relative">
-      <div className="absolute top-4 right-0">
+    <>
+      <Meteors />
+      <div className="absolute top-4 right-4">
         <ThemeToggleButton />
       </div>
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start overflow-hidden p-2">
-        <Meteors />
-        <Profile />
-        <Background />
-      </main>
-    </div>
+      <div className="w-8/12 mx-auto items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 relative">
+        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start overflow-hidden p-2">
+          <Profile />
+          <Background />
+          <Skills />
+        </main>
+      </div>
+    </>
   );
 }
--- components/main/Profile.jsx
+++ components/main/Profile.jsx
@@ -3,11 +3,12 @@ import Image from "next/image"; // Correct import for Next.js optimized images
 import profilePic from "@/public/images/profile.png"; // Import the image
 import { Contacts } from "@/components/main/Contacts";
 import { ThemeToggleButton } from "@/components/ThemeToggleButton";
+import { Meteors } from "@/components/ui/meteors";
 
 export const Profile = () => {
   return (
-    <div className="flex gap-4">
-      <div className="flex flex-col gap-4 w-8/12">
+    <div className="flex gap-4 overflow-hidden">
+      <div className="flex flex-col gap-4 w-8/12 relative">
         <h1 className="text-5xl font-bold">I'm Anan M.</h1>
         <Contacts />
         <p className="text-base text-gray-700 dark:text-gray-200">
@@ -16,7 +17,6 @@ export const Profile = () => {
           commodi optio iure, nemo beatae, harum reprehenderit omnis laboriosam
           magni. Tempore, eligendi.
         </p>
-        
       </div>
       <div className="w-4/12">
         <Image
