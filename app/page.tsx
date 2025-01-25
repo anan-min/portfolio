116e82d9435a6235f57f9d0af3fb83dc6e7fce29
anan <nut999anan@hotmail.com>
2025-01-25 17:44:28 +0700

    update toggle button and links

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,14 @@
 import { Profile } from "@/components/main/Profile";
 import { Meteors } from "@/components/ui/meteors";
 import { Background } from "@/components/main/Background";
+import { ThemeToggleButton } from "@/components/ThemeToggleButton";
 
 export default function Home() {
   return (
     <div className="w-8/12 mx-auto items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 relative">
+      <div className="absolute top-4 right-0">
+        <ThemeToggleButton />
+      </div>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start overflow-hidden p-2">
         <Meteors />
         <Profile />
--- components/main/Profile.jsx
+++ components/main/Profile.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image"; // Correct import for Next.js optimized images
 import profilePic from "@/public/images/profile.png"; // Import the image
+import { Contacts } from "@/components/main/Contacts";
 import { ThemeToggleButton } from "@/components/ThemeToggleButton";
 
 export const Profile = () => {
@@ -8,13 +9,14 @@ export const Profile = () => {
     <div className="flex gap-4">
       <div className="flex flex-col gap-4 w-8/12">
         <h1 className="text-5xl font-bold">I'm Anan M.</h1>
-        <ThemeToggleButton />
+        <Contacts />
         <p className="text-base text-gray-700 dark:text-gray-200">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem ab
           sapiente dignissimos pariatur sint, itaque non explicabo impedit
           commodi optio iure, nemo beatae, harum reprehenderit omnis laboriosam
           magni. Tempore, eligendi.
         </p>
+        
       </div>
       <div className="w-4/12">
         <Image
