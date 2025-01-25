54b00d8dd04d872afdd0af35b29e64a9547cf6e1
nut <nut999anan@hotmail.com>
2025-01-25 16:47:02 +0700

    update profile

--- components/main/Profile.jsx
+++ components/main/Profile.jsx
@@ -1,15 +1,23 @@
 import React from "react";
-import { Contacts } from "./contacts";
+import Image from "next/image"; // Correct import for Next.js optimized images
+import profilePic from "@/public/images/profile.png"; // Import the image
 
-const Profile = () => {
+export const Profile = () => {
   return (
-    <div>
-      <h1>Anan Mingmitpattanakul</h1>
-      <p>Full stack Developer</p>
-      <p></p>
-      <Contacts />
+    <div className="flex gap-4">
+      <div className="flex flex-col gap-4">
+        <h1 className="text-4xl font-bold">Anan Mingmitpattanakul</h1>
+        <p className="text-sm text-gray-800">Full Stack Developer</p>
+        <p className="text-base text-gray-700">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem ab
+          sapiente dignissimos pariatur sint, itaque non explicabo impedit
+          commodi optio iure, nemo beatae, harum reprehenderit omnis laboriosam
+          magni. Tempore, eligendi.
+        </p>
+        <div>
+          <Image src={profilePic} alt="Profile" width={100} height={100} />
+        </div>
+      </div>
     </div>
   );
 };
-
-export default Profile;
